c55a8ad4cf1efffe20e351494735d017a95975d4
Ricardo Marques <marques.ricardo17@gmail.com>
2019-09-30 20:17:51 +0200

    Update Operators.py

--- Introduction/Operators.py
+++ Introduction/Operators.py
@@ -3,3 +3,26 @@ condition = True
 is_not_condition = not condition
 
 print(is_not_condition)
+
+# arithmetic
+# +     Sum
+# -     Subtraction
+# *     Product
+# /     Division
+
+# %     Modulus (Remainder of division)
+# //    Floor division
+
+# +=    
+# -=
+# *=
+# /=
+# **    Exponent operator
+
+# Conditional Operators
+# >     Greater than
+# >=    Greater than or equal to
+# <     Lesser than
+# <=    Smaller than or equal to
+# !=    Is not equal too
+# ==    Is equal too
